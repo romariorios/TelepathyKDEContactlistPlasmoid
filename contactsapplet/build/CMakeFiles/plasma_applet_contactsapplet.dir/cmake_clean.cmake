1e2ebc0f67a77a43be5e73b7221fa4cf6b84affd
Luiz Romário Santana Rios <luizromario@gmail.com>
2010-10-05 23:44:39 -0300

    Wrong contacts-applet folder.

---
+++ contactsapplet/build/CMakeFiles/plasma_applet_contactsapplet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "plasma_applet_contactsapplet_automoc.cpp"
+  "CMakeFiles/plasma_applet_contactsapplet.dir/plasma_applet_contactsapplet_automoc.o"
+  "CMakeFiles/plasma_applet_contactsapplet.dir/contactsapplet.o"
+  "lib/plasma_applet_contactsapplet.pdb"
+  "lib/plasma_applet_contactsapplet.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/plasma_applet_contactsapplet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ contactsapplet/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kde4/kdesvn/contactsapplet
+# Build directory: /home/kde4/kdesvn/contactsapplet/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
